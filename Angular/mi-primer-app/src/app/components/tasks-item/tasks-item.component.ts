b53f81c9ab5c81260ad21026b44e6571fc69f979
Brenda <brendateri@gmail.com>
2022-02-24 11:21:07 -0300

    Componente de lista con items

--- Angular/mi-primer-app/src/app/components/tasks-item/tasks-item.component.ts
+++ Angular/mi-primer-app/src/app/components/tasks-item/tasks-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+//importamos el pack de estilos que descargamos de angular
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tasks-item',
@@ -11,6 +13,8 @@ export class TasksItemComponent implements OnInit {
   //traemos la primer tarea de muestra
   @Input() task: Task = TASKS[0]
 
+  //lo iniciamos
+  faTimes = faTimes;
   constructor() { }
 
   ngOnInit(): void {
